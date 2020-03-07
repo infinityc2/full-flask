63f3ad6d2c6c162bf3a99a9670090d8a11d8871f
infinityc2 <super_star@hotmail.co.th>
2020-03-07 15:33:09 +0700

    config environment :tada:

--- application/__init__.py
+++ application/__init__.py
@@ -4,6 +4,6 @@ import pymysql
 
 app = Flask(__name__)
 app.config.from_object(Config)
-db = pymysql.connect("localhost", "xxx", "xxx", "xxx")
+db = pymysql.connect(Config.MYSQL_HOSTNAME, Config.MYSQL_USERNAME, Config.MYSQL_PASSWORD, Config.MYSQL_DATABASE)
 
 from application import routes
--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 import os
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret_string'
-    
+    SECRET_KEY = os.environ.get('SECRET_KEY') or os.urandom(16)
+    MYSQL_HOSTNAME = os.environ.get("MYSQL_HOSTNAME") or 'localhost'
+    MYSQL_DATABASE = os.environ.get('MYSQL_DATABASE')
+    MYSQL_USERNAME = os.environ.get('MYSQL_USERNAME') or 'root'
+    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD') or ''
