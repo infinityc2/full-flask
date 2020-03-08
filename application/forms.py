0130f38208adf44d848e6a6b7491b6941f20c9f2
infinityc2 <super_star@hotmail.co.th>
2020-03-08 18:15:24 +0700

    create register form and methods

--- application/forms.py
+++ application/forms.py
@@ -1,15 +1,24 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, EqualTo, ValidationError
+from application import db
 
 class LoginForm(FlaskForm):
     username = StringField('username', validators=[DataRequired()])
     password = PasswordField('password', validators=[DataRequired()])
     submit = SubmitField('Login')
 
-class FilterForm(FlaskForm):
-    username = StringField('Username'),
-    first_name = StringField('First Name')
-    last_name = StringField('Last Name')
-    role = StringField('Role')
-    submit = SubmitField('Filter')
+class RegisterForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password_confirm = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    first_name = StringField('First Name', validators=[DataRequired()])
+    last_name = StringField('Last Name', validators=[DataRequired()])
+    submit = SubmitField("Register")
+
+    def validate_username(self, username):
+        with db.cursor() as cursor:
+            cursor.execute(f'SELECT * FROM user WHERE username = "{username.data}"')
+            result = cursor.fetchone()
+            if result:
+                raise ValidationError("Username is already exists")
